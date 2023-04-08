bac1f88387adc3c65c4f317d67710905c81a5388
Jeffmi007 <rombikhh@gmail.com>
2023-04-08 15:37:13 +0300

    points

--- main.py
+++ main.py
@@ -24,6 +24,10 @@ class GameSprite(sprite.Sprite):
        window.blit(self.image, (self.rect.x, self.rect.y))
  
 class Player(GameSprite):
+   def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+       super().__init__(player_image, player_x, player_y, player_speed, wight, height)
+       self.points = 0
+
    def update_r(self):
        keys = key.get_pressed()
        if keys[K_UP] and self.rect.y > 5:
@@ -37,7 +41,7 @@ class Player(GameSprite):
        if keys[K_s] and self.rect.y < win_height - 80:
            self.rect.y += self.speed
  
-background = (200, 255, 255)
+background = (5, 155, 193)
 win_width = 600
 win_height = 500
 window = display.set_mode((win_width, win_height))
@@ -49,7 +53,7 @@ clock = time.Clock()
 FPS = 60
  
  
-ball = GameSprite("tennis_ball.png", 300, 250, 10, 50, 50)
+ball = GameSprite("basketball_ball.png", 300, 250, 10, 50, 50)
 racket1 = Player("racket.png", 30, 200, 4, 50, 150)
 racket2 = Player("racket.png", 520, 200, 4, 50, 150)
 
@@ -60,15 +64,15 @@ while run:
 
     window.fill(background)
    
-    text1 = score_text.render("Рахунок: " + str(score), 1, (0, 0, 0))
+    text1 = score_text.render(f"Рахунок: {racket1.points}", 1, (0, 0, 0))
     window.blit(text1, (10, 20))
-    text2 = lost_text.render("Пропущено: " + str(lost), 1, (0, 0, 0))
+    text2 = lost_text.render(f"Рахунок: {racket2.points}", 1, (0, 0, 0))
     window.blit(text2, (10, 50))
 
     for e in event.get():
+        
         if e.type == QUIT:
             run = False
-
 
     ball.reset()
     racket1.reset()
@@ -81,10 +85,17 @@ while run:
 
     if sprite.collide_rect(racket1, ball) or sprite.collide_rect(racket2, ball):
         speed_x *= -1
-        speed_y *= 1
 
     if ball.rect.y <= 0 or ball.rect.y >= win_height - ball.rect.height:
         speed_y *= -1 
+
+    if ball.rect.x <= 0:
+        racket1.points -= 1
+    if ball.rect.x >= win_width - ball.rect.width:
+        racket2.points -= 1
         
+    
+
+
     display.update()
     clock.tick(FPS)
